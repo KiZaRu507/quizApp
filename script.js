4b4be5963e8a3077ffa5d0d415e9884d047cd736
Sumukh VR <vrsumukh32@gmail.com>
2021-09-17 18:02:14 +0530

    Firstcommitforquiz

---
+++ script.js
@@ -0,0 +1,33 @@
+const questions=[{
+    question:'What is my name?',
+    a:'Sumukh',
+    b:'KiZaRu',
+    c:'John',
+    d:'Harvey',
+    answer:'Sumukh'
+},
+{
+    question:'Is Python An Object oriented language',
+    a:'Yes',
+    b:'No',
+    c:'There is no such thing',
+    d:'I dont care',
+    answer:'Yes'
+
+},
+{
+    question:'Who is the father of computers?',
+    a:'Charles Babbage',
+    b:'KiZaRu',
+    c:'My dad',
+    d:'Fuck its Kim jong un',
+    answer:'Charles Babbage'
+},
+{
+    question:'Are you vaccinated?',
+    a:'Yes',
+    b:'No',
+    c:'Im waiting for an appointment ',
+    d:'I dont believe in vaccines',
+    answer:'Yes'
+}]
